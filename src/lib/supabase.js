b5ea214cad67887d8b5f0639c8f11d437d3bcdb7
lmgomeze77 <42914754+lmgomeze77@users.noreply.github.com>
2026-05-26 10:24:55 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL  || null;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY || null;
+const url = import.meta.env.VITE_SUPABASE_URL  || "https://jpecmcplicwhmtfnbpdi.supabase.co";
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZWNtY3BsaWN3aG10Zm5icGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NzQxOTksImV4cCI6MjA5MTE1MDE5OX0.lqwOc850MbdmW2lZKwLCRESHWOHV1Sk1sFXUtYcn1Q4";
 
-export const supabase = (url && key) ? createClient(url, key) : null;
+export const supabase = createClient(url, key);
